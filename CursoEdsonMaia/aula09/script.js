cceacaecb009da4b666755c823364cbcd7918595
EduBGou <136281205+EduBGou@users.noreply.github.com>
2023-12-11 19:34:44 -0300

    update

--- CursoEdsonMaia/aula09/script.js
+++ CursoEdsonMaia/aula09/script.js
@@ -2,8 +2,15 @@ function fThemeMode(tag, mode){
     let obj = document.querySelector(`${tag}`).classList
     console.log(tag, mode)
 
-   obj.forEach(function(className){
+    obj.forEach(function(className){
         obj.remove(className)
     })
+
+    /*
+    for(let i = 0; i < obj.length; i++){
+        obj.remove(obj[i])
+    }
+    */
+   
     obj.add(`${mode}`)
 }
