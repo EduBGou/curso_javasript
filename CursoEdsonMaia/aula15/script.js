d1161711ff107f6b735585cf4b9d7957e8c2c50e
EduBGou <136281205+EduBGou@users.noreply.github.com>
2023-12-25 13:26:56 -0300

    up

--- CursoEdsonMaia/aula15/script.js
+++ CursoEdsonMaia/aula15/script.js
@@ -1,17 +1,46 @@
-
+function vldNum(obj){
+    let num = obj.value
+    let elementAlert = document.querySelector('#alert')
+    if(num < 0 || num > 10 || num == ''){
+        elementAlert.textContent = 'Inisra um Valor Válido'
+        elementAlert.className = 'red'
+    }else{
+        console.log(num)
+        elementAlert.textContent = ''
+        elementAlert.className = ''
+    }
+}
 
 let bxGrade01 = document.querySelector('#grade01')
 let bxGrade02 = document.querySelector('#grade02')
 
-function vldNum(){
-    num01 = bxGrade01.value
-    num02 = bxGrade02.value
+let bttnCalc = document.querySelector('#bttnCalc')
+let bttnClean = document.querySelector('#bttnClean')
 
-    if(num01 < 0 || num01 > 10 || num02 < 0 || num02 > 10 || num01 == '' || num02 == ''){
-        let alert = document.querySelector('#alert')
-        alert.textContent = 'Inisra um valor Válido'
+let inputAvrg = document.querySelector('#avrg')
+let situation = document.querySelector('#situation')
+
+bttnCalc.addEventListener('click', () => {
+    let num01 = Number(bxGrade01.value)
+    let num02 = Number(bxGrade02.value)
+    let avrg = (num01 + num02) / 2
+
+    inputAvrg.value = avrg
+
+    if (avrg >= 7){
+        situation.textContent = 'Aprovado!'
+    }else if (avrg < 5){
+        situation.textContent = 'Reprovado!'
     }else{
-        console.log(num01, num02)
-        alert.textContent = ''
+        situation.textContent = 'Recuperação.'
     }
-}
+
+})
+
+bttnClean.addEventListener('click', () => {
+    bxGrade01.value = ''
+    bxGrade02.value = ''
+
+    inputAvrg.value = ''
+    situation.textContent = 'Situação do Estudante'
+})
